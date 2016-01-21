af6ab6c239254492d26a8179127d344713b1771a
Toby Kao <toby850817@gmail.com>
2016-01-21 18:54:47 +0800

    Add ViewModel

---
+++ SenkaKichi/Models/ViewModels/ControllerViewModels/ServerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SenkaKichi.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SenkaKichi.ViewModels.Server
+{
+    public class RankingViewModel
+    {
+        public List<SenkaData> Data { get; set; }
+        public DbModels.Server Server { get; set; }
+        public PagerViewModels Pager { get; set; }
+    }
+
+    public class InfoViewModel
+    {
+        public DbModels.Server Server { get; set; }
+        public DateInfo DateInfo { get; set; }
+        public string JsonChart { get; set; }
+    }
+}
